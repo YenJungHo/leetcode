7ba01446699cef579e611899ac96d3fe240fbf09
Raymond Ho <mondray1028@gmail.com>
2019-06-01 13:17:56 +0800

    Commit leetcode Alg. 09  Palindrome Number

---
+++ algorithms/09_PalindromeNumber/PalindromeNumber/PalindromeNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeNumber
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			Solution sol = new Solution();
+			bool res = sol.IsPalindrome( 121 );
+			Console.WriteLine( res );
+			Console.Read();
+
+		}
+	}
+}
---
+++ algorithms/09_PalindromeNumber/PalindromeNumber/PalindromeNumber/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeNumber
+{
+	public class Solution
+	{
+		public bool IsPalindrome( int x )
+		{
+			if( x < 0 || ( ( x % 10 == 0 ) && ( x != 0 ) ) ) {
+				return false;
+			}
+			int bak = x;
+
+			int reverse = 0;
+			int pop = 0;
+			while( x != 0 ) {
+				pop = x % 10;
+				x /= 10;
+				reverse = reverse * 10 + pop;
+			}
+
+			return reverse == bak;
+		}
+	}
+}
